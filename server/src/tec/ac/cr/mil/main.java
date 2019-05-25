708325e5d25ec9bd9ca5dfc5d4105f4aa6d8bca3
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2019-05-25 15:02:29 -0600

    Commit Inicial con el server

---
+++ server/src/tec/ac/cr/mil/main.java
@@ -0,0 +1,9 @@
+package tec.ac.cr.mil;
+
+public class main {
+
+    public static void main(String[] args){
+        System.out.println("Hola");
+    }
+
+}
---
+++ server/src/tec/ac/cr/mil/server/MainServer.java
@@ -0,0 +1,8 @@
+package tec.ac.cr.mil.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("library")
+public class MainServer extends Application {
+}
