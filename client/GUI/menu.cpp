5da6acb50d89117aceb71d52d68f3eef77b12d9d
alejandro <josealejandroibarra@hotmail.com>
2019-05-28 23:40:46 -0600

    Now with more assets and drag & drop support for files.

--- client/GUI/menu.cpp
+++ client/GUI/menu.cpp
@@ -5,11 +5,43 @@ Menu::Menu(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::Menu)
 {
+    // Initalize default parameters
     ui->setupUi(this);
-    ui->background->setStyleSheet("QLabel { background-color : red; color : white; }");
+    setAcceptDrops(true);
+
+    // Button hover watchers
+    ButtonHoverWatcher* addWatcher = new ButtonHoverWatcher(this, ":/main/buttons/buttons/addButton.png",":/main/buttons/buttons/addButton_hovered.png");
+    ui->addButton->installEventFilter(addWatcher);
 }
 
 Menu::~Menu()
 {
     delete ui;
+}
+
+//! Executes while dragging file to window
+void Menu::dragEnterEvent(QDragEnterEvent* e) {
+    if (e->mimeData()->hasUrls()) {
+        e->acceptProposedAction();
+    }
+}
+
+//! Executes when dropping file in window
+void Menu::dropEvent(QDropEvent* e) {
+    QStringList accepted_types;
+    accepted_types << "jpeg" << "jpg" << "png" << "heif" << "bmp";
+    foreach(const QUrl & url, e->mimeData()->urls()) {
+        QString fileName = url.toLocalFile();
+        QFileInfo info(fileName);
+        if (info.exists()) {
+            if (accepted_types.contains(info.suffix().trimmed(), Qt::CaseInsensitive))
+                qDebug() << fileName;
+        }
+    }
+}
+
+void Menu::on_addButton_clicked() {
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Image"), "/home/jana", tr("Image Files (*.jpeg *.jpg *.png *.heif *.bmp)"));
+    qDebug() << fileName;
 }
--- client/GUI/menu.h
+++ client/GUI/menu.h
@@ -1,8 +1,15 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <QDebug>
+#include <QDragEnterEvent>
+#include <QFileDialog>
+#include <QFileInfo>
 #include <QMainWindow>
+#include <QMimeData>
 #include <string>
+
+#include "elements/buttonhoverwatcher.h"
 
 using namespace std;
 
@@ -19,7 +26,12 @@ public:
     explicit Menu(QWidget *parent = nullptr);
     ~Menu();
 
+protected:
+    void dragEnterEvent(QDragEnterEvent *e);
+    void dropEvent(QDropEvent *e);
+
 private slots:
+    void on_addButton_clicked();
 
 private:
 
