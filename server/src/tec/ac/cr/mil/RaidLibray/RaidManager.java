41cf42c1438fef235e31de311f565075d710790c
[Fabian] <[ramirezfabian003@gmail.com]>
2019-05-30 19:41:30 -0600

    Creado archivo de paridad al hacer write

--- server/src/tec/ac/cr/mil/RaidLibray/RaidManager.java
+++ server/src/tec/ac/cr/mil/RaidLibray/RaidManager.java
@@ -8,35 +8,43 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class RaidManager {
+
+    private static int parityPosition = 0;
+
     public static void Write(byte[] ImageBytes, String ImageName) throws IOException {
-        int fileSize = 10000;
         int length = ImageBytes.length;
+        int fileSize = length/4;
         int fileCounter = 0;
         int fileTotal = length/fileSize;
         int currentDisk = 0;
         while (fileCounter < fileTotal){
-            int begin = fileCounter*fileSize;
-            int end = (fileCounter+1)*fileSize;
+            if(currentDisk != parityPosition) {
+                int begin = fileCounter * fileSize;
+                int end = (fileCounter + 1) * fileSize;
 
-            if(fileCounter != 0) end++;
-            if(fileCounter == fileTotal-1) end = length;
+                if (fileCounter != 0) end++;
+                if (fileCounter == fileTotal - 1) end = length;
 
-            byte[] partition = Arrays.copyOfRange(ImageBytes, begin, end);
+                byte[] partition = Arrays.copyOfRange(ImageBytes, begin, end);
 
-            fillFile(partition, fileCounter, ImageName, currentDisk);
+                fillFile(partition, fileCounter, ImageName, currentDisk);
 
-            fileCounter++;
+                fileCounter++;
 
+                if (currentDisk == 5) currentDisk = 0;
+            }
             currentDisk++;
-            if (currentDisk == 5) currentDisk = 0;
         }
-
+        if(parityPosition == 5) parityPosition = 0;
+        calcParity(fileSize, ImageName, parityPosition);
+        parityPosition++;
 
     }
     public static byte[] Read(int size, String Name){
         byte[] FileBytes = new byte[size];
+        int stripSize = size/5;
         int currentDisk = 0;
-        int totalPartitions = size/10000;
+        int totalPartitions = size/stripSize;
         int counter = 0;
 
         while (counter < totalPartitions){
@@ -46,7 +54,7 @@ public class RaidManager {
                     "Disks"+File.separator+"d"+currentDisk+File.separator+Name+counter+".pdf");
             try {
                 byte[] bArray = Files.readAllBytes(path);
-                System.arraycopy(bArray, 0, FileBytes, (10000 * counter), bArray.length);
+                System.arraycopy(bArray, 0, FileBytes, (stripSize * counter), bArray.length);
                 // reading content from byte array
             } catch (IOException e) {
                 e.printStackTrace();
@@ -64,7 +72,7 @@ public class RaidManager {
 
     private static void fillFile(byte[] byteFiles, int index, String fileName, int currentDisk){
         File file = new File("."+File.separator+"server"+File.separator+
-                "src"+File.separator+"tec"+File.separator+"ac+File.pathSeparator"+File.separator+
+                "src"+File.separator+"tec"+File.separator+"ac"+File.separator+
                 "cr"+File.separator+"mil"+File.separator+"RaidLibray"+File.separator+"Disks"+File.separator+
                 "d"+currentDisk+File.separator+fileName+index+".pdf");
         try (FileOutputStream fos = new FileOutputStream(file)) {
@@ -76,4 +84,62 @@ public class RaidManager {
         }
     }
 
+    private static void calcParity(int size, String fileName, int disk){
+        File file = new File("."+File.separator+"server"+File.separator+
+                "src"+File.separator+"tec"+File.separator+"ac"+File.separator+
+                "cr"+File.separator+"mil"+File.separator+"RaidLibray"+File.separator+"Disks"+File.separator+
+                "d"+parityPosition+File.separator+fileName+"Parity.pdf");
+
+        byte[] parity = calcByteArrayParity(size, fileName, disk);
+        try (FileOutputStream newFile = new FileOutputStream(file)) {
+            newFile.write(parity);
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    private static byte[] calcByteArrayParity(int size, String fileName, int disk){
+        byte[] Response = new byte[size];
+        byte[][] BytesMatrix = calcByteMatrix(size, fileName, disk);
+        int i = 0;
+        int y = 0;
+        byte sum = 0;
+        while(i<size){
+            while (y<5 && y!=parityPosition){
+                sum += (byte) + BytesMatrix[y][i];
+                y++;
+            }
+            Response[i] = sum;
+            y=0;
+            i++;
+        }
+
+
+
+        return Response;
+    }
+
+    private static byte[][] calcByteMatrix (int size, String fileName, int disk){
+        byte[][] Response = new byte[4][size];
+        int counter = 0;
+        while (counter<5 && counter!= parityPosition){
+            Path path = Paths.get("."+File.separator+"server"+File.separator+"src"+
+                    File.separator+"tec"+File.separator+"ac"+File.separator+"cr"
+                    +File.separator+"mil"+File.separator+"RaidLibray"+File.separator+
+                    "Disks"+File.separator+"d"+counter+File.separator+fileName+counter+".pdf");
+            try {
+                byte[] bArray = Files.readAllBytes(path);
+                Response[counter]=bArray;
+                // reading content from byte array
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            counter++;
+        }
+        return Response;
+    }
 }
