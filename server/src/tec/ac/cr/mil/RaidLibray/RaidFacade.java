e16eb5aa0a0c3ecc338d71ed60823ae2e9f10025
[Fabian] <[ramirezfabian003@gmail.com]>
2019-06-11 16:29:34 -0600

    Iniciado RaidFacade

---
+++ server/src/tec/ac/cr/mil/RaidLibray/RaidFacade.java
@@ -0,0 +1,23 @@
+package tec.ac.cr.mil.RaidLibray;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class RaidFacade {
+    public static void addImage(String imageString, String name) throws IOException {
+        byte[] bytes = imageString.getBytes();
+        RaidManager.Write(bytes, name);
+        //Add to DataBase
+    }
+    public static void deleteImage(String imageName){
+        RaidManager.deleteImage(imageName);
+        //Delete dataBase
+    }
+    public static void readImage(int imageSize, String imageName){
+        byte[] bytes = RaidManager.Read(imageSize, imageName);
+        String imageString = Arrays.toString(bytes);
+    }
+    public static void readAllImages(){
+        //recursive method
+    }
+}
