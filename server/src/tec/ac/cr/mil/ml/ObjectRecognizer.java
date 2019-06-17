68174ba23efc42dc697970847b10dc3103a9b734
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2019-06-16 22:02:23 -0600

    Bugfix

--- server/src/tec/ac/cr/mil/ml/ObjectRecognizer.java
+++ server/src/tec/ac/cr/mil/ml/ObjectRecognizer.java
@@ -63,11 +63,7 @@ public class ObjectRecognizer {
      */
     public String getDescription(byte[] imageBytes) {
         // Define ML model
-        File file = new File("." + File.separator + "server" +
-                File.separator + "src" + File.separator + "tec" +
-                File.separator + "ac" + File.separator + "cr" +
-                File.separator + "mil" + File.separator + "ml" +
-                File.separator + "model");
+        File file = new File("C:\\Users\\Kevin Cordero Zúñiga\\IdeaProjects\\MyInvincibleLibrary\\server\\src\\tec\\ac\\cr\\mil\\ml\\model");
         modelpath = file.getAbsolutePath();
         graphDef = ObjectRecognizer.readBytes(Paths.get(modelpath,"tensorflow_inception_graph.pb"));
         labels = ObjectRecognizer.readLines(Paths.get(modelpath, "imagenet_comp_graph_label_strings.txt"));
