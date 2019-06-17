a266b183820845424823496f9c1d647e2971b04a
AlejandroI <josealejandroibarra@hotmail.com>
2019-06-16 19:06:43 -0600

    Now with perfectly spelled comments.

--- server/src/tec/ac/cr/mil/main.java
+++ server/src/tec/ac/cr/mil/main.java
@@ -1,37 +1,9 @@
 package tec.ac.cr.mil;
 
-import tec.ac.cr.mil.logic.DatabaseFacade;
-import tec.ac.cr.mil.logic.Picture;
-import tec.ac.cr.mil.logic.Serializer;
-import tec.ac.cr.mil.raid.RaidFacade;
-
-import java.io.IOException;
-
 public class main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        // RAID Stuff:
-
-        //BufferedImage bImage = ImageIO.read(new File("C:\\Users\\ramir\\Pictures\\prueba.jpeg"));
-        //ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        //ImageIO.write(bImage, "jpeg", bos);
-        //byte [] data = bos.toByteArray();
-        //RaidManager.Write(data,"prueba");
-       // byte[] response = RaidManager.Read(92086, "prueba");
-        //System.out.println(data[90085]);
-       /// System.out.println(response[9]);
-
-        // Object recognition using Machine Learning:
-
-       /* ObjectRecognizer r = new ObjectRecognizer();
-        File loadedImg = new File( "." + File.separator + "server" +
-                File.separator + "src" + File.separator + "tec" +
-                File.separator + "ac" + File.separator + "cr" +
-                File.separator + "mil" + File.separator + "testresources" +
-                File.separator + "apple.jpeg");
-        String description = r.getDescription(loadedImg);
-        System.out.println(description);*/
     }
 
 }
